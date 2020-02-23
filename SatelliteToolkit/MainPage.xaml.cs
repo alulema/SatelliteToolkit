ae839b117ae54a5fb577f2a02244030cf860cf3e
Alexis Alulema <alexis.alulema@gmail.com>
2020-02-23 08:09:34 -0500

    First commit

---
+++ SatelliteToolkit/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SatelliteToolkit
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            hamburgerButton.ImageSource = (FileImageSource)ImageSource.FromFile("hamburger_icon.png");
+        }
+
+        void hamburgerButton_Clicked(object sender, EventArgs e)
+        {
+            navigationDrawer.ToggleDrawer();
+        }
+    }
+}
